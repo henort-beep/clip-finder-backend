4c6650ddf85fa78da968a4f67ad619efbaa5feec
henort-beep <heitorramosna@gmail.com>
2026-04-23 20:31:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const https = require("https");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 function extractVideoId(url) {
@@ -44,6 +44,12 @@ async function getTranscript(videoId) {
     .join(" ");
   return texts;
 }
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({ status: "ok", message: "Clip Finder Backend rodando!" });
